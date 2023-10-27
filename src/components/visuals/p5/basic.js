28c2ce188c6d60e52eaaa2aec1c4346c03cd09ed
Esteban Romero <73259884+esromerog@users.noreply.github.com>
2023-10-27 15:36:49 -0400

    Fixed sun visualization, improved Circle

--- src/components/visuals/p5/basic.js
+++ src/components/visuals/p5/basic.js
@@ -8,7 +8,11 @@ p.setup = () => {
 // Code that is constantly being updates
 p.draw = () => {
     // Sets your background color to black using RGB values
+    // The values go from a range between 0 and 255 (8-bit color)
     p.background(0, 0, 0);
+
+    // Sets the color of the circle using also RGB
+    p.fill(255, 255, 255);
 
     // Declare your size variable that is based on your size parameter
     // All parameters go between 0 and 1, so you need to scale them as desired.
